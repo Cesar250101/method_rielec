5b7ca0e32713bee3fc5b79d04740b637de4431d5
Cesar250101 <cesar250101@outlook.com>
2022-10-27 14:05:55 -0300

    Actualizar costo desde las OC

--- models/models.py
+++ models/models.py
@@ -2,16 +2,28 @@
 
 from odoo import models, fields, api
 
+
+class Compras(models.Model):
+    _inherit = 'purchase.order'
+
+    @api.model
+    def _actualizar_costo_desde_oc(self):
+        for l in self.order_line:
+            product=self.env['product.product'].search([('id','=',l.product_id.id)])
+            values={
+                'standard_price':l.price_unit
+            }        
+            product.write(values)
+
+
 class ComprobantesLineas(models.Model):
     _inherit = 'account.move.line'
     _description = 'Comprobantes contables detalle'
-    
-    
+        
     @api.model
     def _compute_fecha_vcto(self):
         if self.account_id.calcula_vcto:
             self.date_maturity=self.date_maturity+self.partner_id.property_payment_term_id.line_ids.days
-
     
 class PlanCuenta(models.Model):
     _inherit = 'account.account'
